c8626eda93af8f10e0585c58827c91bdf8c65741
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-03-17 11:32:58 +0100

    feat: add fontawesome

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
 
+import { faChessKnight } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  ngInit() {
-  }
-
-  toggleTheme() {
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-  }
+  faChessKnight = faChessKnight;
 }
