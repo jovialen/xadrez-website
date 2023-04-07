9318d8ecfc43616a5bc96128a45e920c601b88a0
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-04-07 17:17:12 +0200

    fix: add plugin to allow for top level await

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import wasm from 'vite-plugin-wasm'
+import topLevelAwait from "vite-plugin-top-level-await";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		wasm()
+		wasm(),
+		topLevelAwait({
+			promiseExportName: '__tla',
+			promiseImportName: i => `__tla_${i}`
+		})
 	]
 });
